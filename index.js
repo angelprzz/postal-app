a9c599e3845320198611857e0e3eab102b0dbfad
Ángel Pérez <angelpxrz@gmail.com>
2020-11-22 18:21:41 +0100

    Set up database

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const Post = require('./models/Post');
 const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch(err) {
+                throw new Error(err);
+            }
+        }
     }
 }
 
@@ -21,8 +35,7 @@ const server = new ApolloServer({
     resolvers
 });
 
-mongoose
-    .connect(MONGODB, { userNewUrlParser: true })
+mongoose.connect(MONGODB, { userNewUrlParser: true })
     .then(() => {
         console.log('MongoDB Connected')
         return server.listen({  port: 3000 })
