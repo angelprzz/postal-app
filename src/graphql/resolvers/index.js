2951a6c0b58d7b285ec5b8b8644db9d2dc51fe87
Ángel Pérez <angelpxrz@gmail.com>
2020-12-09 15:02:10 +0100

    Set Like & Comment Count

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -3,6 +3,10 @@ const postsResolvers = require('./posts')
 const commentsResolvers = require('./comments')
 
 module.exports = {
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
