c4fbf26ad2c4d4b0c01af45fdda00bc2acc3bf65
Ángel Pérez <angelpxrz@gmail.com>
2020-12-09 11:40:32 +0100

    Update project structure

--- src/graphql/resolvers/users.js
+++ src/graphql/resolvers/users.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 
 const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
-const { SECRET_KEY } = require('../../config')
+const { SECRET_KEY } = require('../../../config')
 const User = require('../../models/User')
 
 function generateToken(user) {
