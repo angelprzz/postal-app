99c36aa9b861bd01ab5bbfce7ecae0eae671e6b9
Ángel Pérez <angelpxrz@gmail.com>
2020-12-11 15:27:40 +0100

    Organize Bar code & fix typo

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { AuthContext } from '../context/auth'
 
 function MenuBar() {
-    const { user, logout } = useContext(AuthContext)
+  const { user, logout } = useContext(AuthContext)
   const pathname = window.location.pathname;
 
   const path = pathname === '/' ? 'home' : pathname.substr(1);
@@ -15,18 +15,10 @@ function MenuBar() {
 
     const menuBar = user ? (
         <Menu pointing secondary size="massive" color="teal">
-            <Menu.Item
-                name='user.username'
-                active
-                as={Link}
-                to="/"
-            />
+            <Menu.Item name={user.username} active as={Link} to="/"/>
 
             <Menu.Menu position='right'>
-            <Menu.Item
-                name='logout'
-                onClick={logout}
-            />
+                <Menu.Item name='logout' onClick={logout} />
             </Menu.Menu>
         </Menu>
     ) : (
@@ -40,25 +32,25 @@ function MenuBar() {
             />
 
             <Menu.Menu position='right'>
-            <Menu.Item
-                name='login'
-                active={activeItem === 'login'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/login"
-            />
-            <Menu.Item
-                name='register'
-                active={activeItem === 'register'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/register"
-            />
+                <Menu.Item
+                    name='login'
+                    active={activeItem === 'login'}
+                    onClick={handleItemClick}
+                    as={Link}
+                    to="/login"
+                />
+                <Menu.Item
+                    name='register'
+                    active={activeItem === 'register'}
+                    onClick={handleItemClick}
+                    as={Link}
+                    to="/register"
+                />
             </Menu.Menu>
         </Menu>
     )
 
-    return menubar;
+    return menuBar;
 }
 
 export default MenuBar
