88ecc15c4cfc76464957c079940938551e343193
Ángel Pérez <angelpxrz@gmail.com>
2020-12-12 16:14:32 +0100

    Add transition post && manage errors

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -29,21 +29,31 @@ function PostForm() {
     }
 
     return (
-        <Form onSubmit={onSubmit}>
+        <>
+          <Form onSubmit={onSubmit}>
             <h2>Create a post:</h2>
             <Form.Field>
-                <Form.Input 
-                    placeholder="Postal" 
-                    name="body" 
-                    onChange={onChange} 
-                    value={values.body}
-                />
-                <Button type="submit" color="teal">
-                    Submit
-                </Button>
+              <Form.Input
+                placeholder="Type something"
+                name="body"
+                onChange={onChange}
+                value={values.body}
+                error={error ? true : false}
+              />
+              <Button type="submit" color="teal">
+                Submit
+              </Button>
             </Form.Field>
-        </Form>
-    )
+          </Form>
+          {error && (
+            <div className="ui error message" style={{ marginBottom: 20 }}>
+              <ul className="list">
+                <li>{error.graphQLErrors[0].message}</li>
+              </ul>
+            </div>
+          )}
+        </>
+      );
 }
 
 const CREATE_POST_MUTATION = gql`
